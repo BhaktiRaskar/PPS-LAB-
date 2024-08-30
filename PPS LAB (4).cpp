8536d1a11aef7e7917182ffa69b0b204615a96c3
BhaktiRaskar <bhakti.raskar.7200@gmail.com>
2024-08-30 20:04:14 +0530

    Add files via upload
    it is a program that shows the number is negative or positive ...and if the number is positive the statement "IF" will get an as output.
    if the number is negative then  "ElSE" statement will get as an output. the ./n is given because to complete the statement..

---
+++ PPS LAB (4).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int number ;
+  
+    cout<<"Enter a number:";
+    cin>>number;
+    if (number>=0)
+    {
+        cout<<"the number"<<number<<"is positive./n";
+    }
+    else
+    {
+        cout<<"the number"<<number<<"is negative./n";
+    }
+    return 0;
+}
